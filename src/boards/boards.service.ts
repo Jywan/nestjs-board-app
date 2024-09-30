673461a717558736a550eeb37f353dfd9cfd21b8
장영완 <apdl815@naver.com>
2024-09-30 20:48:28 +0900

    Clone coding of nestjs board-app

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Injectable()
+export class BoardsService {
+    private boards: Board[] = [];
+
+    getAllBoards(): Board[] {
+        return this.boards;
+    }
+
+    createBoard(createBoardDto: CreateBoardDto) {
+        const {title, description} = createBoardDto;
+
+        const board: Board = {
+            id: uuid(), //유니크한 값 주입
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
+    } 
+
+    getBoardById(id: string): Board {
+        return this.boards.find((board) => board.id === id);
+    }
+}
